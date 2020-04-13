5befc51c29095286b124ee0dd0c00aaf1728c88e
Jatin Katyal <jatin.katyal13@gmail.com>
2020-04-13 21:37:47 +0530

    file upload using minio

--- min_io/views.py
+++ min_io/views.py
@@ -1,10 +1,10 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from django.http import HttpResponse
 from django.views.decorators.csrf import requires_csrf_token
 
 from decouple import config
+import time
 
 from minio import Minio
 from minio.error import ResponseError
@@ -20,5 +20,19 @@ minioClient = Minio(
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def upload(request):
-  print(request.FILES)
-  return HttpResponse('something')
+  file = request.FILES['file']
+
+  timestamp = int(time.time())
+  fileName = f'{request.user.id}-{timestamp}-{file.name}'
+  url = f'http{"s" if config("S3_SSL", cast = bool) else ""}://{config("S3_ENDPOINT")}/{config("S3_BUCKET")}/{fileName}'
+
+  resp = minioClient.put_object(
+    bucket_name = config('S3_BUCKET'), 
+    object_name= fileName, 
+    data = file.file,
+    length = file.size,
+    content_type = file.content_type
+  )
+  return Response({
+    'url': url
+  })
